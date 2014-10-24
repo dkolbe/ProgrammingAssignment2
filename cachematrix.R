793a4a26cae0db78a2bc23ad6329668927fa3351
Diana Kolbe <diana.kolbe@gmail.com>
2014-10-24 18:24:55 -0500

    Stub object: Functions in place; cacheSolve checks for first run.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    setMat <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    getMat <- function() x
+    setInv <- function(inverse) inv <<- inverse
+    getInv <- function() inv
+    
+    list(setMat = setMat,
+         getMat = getMat,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInv()
+    if(is.null(inv)) {
+        # No value cached - calculate it
+        inv <- TRUE
+        x$setInv(inv)
+    } else {
+        message("Using cache data for matrix inverse")
+    }
+      
+    inv
+    ## Return a matrix that is the inverse of 'x'
 }
